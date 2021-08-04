4bbb2ded1c4fd1e0a4ed35477e954f89c6ce9de8
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-08-04 17:17:00 +0800

    Setup Events/Release-Notes and Community/Apache-Release
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './Hero.module.css';
-import ServerClusterImg from '../../static/img/undraw-server-cluster.svg';
+import EventMeshImg from '../../static/img/eventmesh.png';
 
 const Hero = (): JSX.Element => {
   const { siteConfig } = useDocusaurusContext();
@@ -42,9 +42,10 @@ const Hero = (): JSX.Element => {
           </div>
 
           <div className={clsx('col col--6', styles.heroImage)}>
-            <div>
-              <ServerClusterImg height="300px" width="100%" />
-            </div>
+            <img
+              src={EventMeshImg}
+              alt="EventMesh"
+            />
           </div>
         </div>
       </div>
