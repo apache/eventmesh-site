ba55e573a84d3348d2d26a62aa0ae07ee30be61c
Xiaoyang Liu <siujoeng.lau@gmail.com>
2022-04-16 16:12:02 -0700

    Migrate the 'fetch.ts' script to TypeScript and update .asf.yaml
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,26 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const simpleGit = require('simple-git');
+import fs from 'fs';
+import path from 'path';
+import simpleGit from 'simple-git';
 
-const rewriteMarkdown = (file) => {
-  let content = fs.readFileSync(file, 'utf-8');
-  content = content.replaceAll(/]\(.*images\//g, '](/images/');
-  content = content.replaceAll('?raw=true', '');
-  content = content.replace('[点我查看中文版](../cn/README.md)', '');
+const rewriteMarkdown = (file: string) => {
+  const content = fs.readFileSync(file, 'utf-8');
   fs.writeFileSync(file, content, 'utf-8');
 };
 
-const copyFolder = (srcDir, tarDir) => {
+const copyFolder = (srcDir: string, tarDir: string) => {
   const files = fs.readdirSync(srcDir);
   if (fs.existsSync(tarDir) === false) {
     fs.mkdirSync(tarDir);
   }
+
   files.forEach((file) => {
     const srcPath = path.join(srcDir, file);
-
-    let fileName = file.replaceAll('.en-US.md', '.md');
-    fileName = file.replaceAll('.zh-CN.md', '.md');
-    const tarPath = path.join(tarDir, fileName);
+    const tarPath = path.join(tarDir, file);
 
     const stats = fs.statSync(srcPath);
     if (stats.isDirectory()) {
@@ -38,20 +33,20 @@ const copyFolder = (srcDir, tarDir) => {
 };
 
 const sync = async () => {
-  console.log('Cloning https://github.com/apache/incubator-eventmesh.git');
+  console.log('[Apache EventMesh Site] Fetch apache/incubator-eventmesh');
   const git = simpleGit();
   await git.clone(
     'https://github.com/apache/incubator-eventmesh.git',
     './tmp/incubator-eventmesh',
   );
 
-  console.log('Extracting docs from apache/incubator-eventmesh');
+  console.log('[Apache EventMesh Site] Extract documentation from apache/incubator-eventmesh');
   await git.cwd('./tmp/incubator-eventmesh').checkout('master', ['-f']);
   copyFolder('./tmp/incubator-eventmesh/docs/images/', './static/images/');
   copyFolder('./tmp/incubator-eventmesh/docs/en/', './docs/');
   copyFolder('./tmp/incubator-eventmesh/docs/cn/', './i18n/zh/docusaurus-plugin-content-docs/current/');
 
-  console.log('Removing tmp/incubator-eventmesh');
+  console.log('[Apache EventMesh Site] Removed tmp/incubator-eventmesh');
   fs.rmSync('./tmp', {
     recursive: true,
   });
