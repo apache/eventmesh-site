4f4dd435be106deb160dae8e887086d31b46b415
SUN <350916525@qq.com>
2023-12-25 12:21:41 +0800

    1. remove Incubation component 2. add Apache logo on page footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -77,34 +77,39 @@ function Footer(): JSX.Element | null {
       <div className="container">
         {links && links.length > 0 && (
           <div className="row footer__links">
-            {links.map((linkItem, i) => (
-              <div key={i} className="col footer__col">
-                {linkItem.title != null ? (
-                  <div className="footer__title">{linkItem.title}</div>
-                ) : null}
-                {linkItem.items != null
-                && Array.isArray(linkItem.items)
-                && linkItem.items.length > 0 ? (
-                  <ul className="footer__items">
-                    {linkItem.items.map((item, key) => (item.html ? (
-                      <li
-                        key={key}
-                        className="footer__item"
-                          // Developer provided the HTML, so assume it's safe.
-                          // eslint-disable-next-line react/no-danger
-                        dangerouslySetInnerHTML={{
-                          __html: item.html,
-                        }}
-                      />
-                    ) : (
-                      <li key={item.href || item.to} className="footer__item">
-                        <FooterLink {...item} />
-                      </li>
-                    )))}
-                  </ul>
+            <div  className="col footer__col">
+              <img src='https://doris.apache.org/images/asf_logo_apache.svg' />
+            </div>
+            {[{logoSrc: ''},...links].map((linkItem, i) => {
+              return (
+                <div key={i} className="col footer__col">
+                  {linkItem.title != null ? (
+                    <div className="footer__title">{linkItem.title}</div>
                   ) : null}
-              </div>
-            ))}
+                  {linkItem.items != null
+                  && Array.isArray(linkItem.items)
+                  && linkItem.items.length > 0 ? (
+                    <ul className="footer__items">
+                      {linkItem.items.map((item, key) => (item.html ? (
+                        <li
+                          key={key}
+                          className="footer__item"
+                            // Developer provided the HTML, so assume it's safe.
+                            // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: item.html,
+                          }}
+                        />
+                      ) : (
+                        <li key={item.href || item.to} className="footer__item">
+                          <FooterLink {...item} />
+                        </li>
+                      )))}
+                    </ul>
+                    ) : null}
+                </div>
+              )
+            })}
           </div>
         )}
         {(logo || copyright) && (
